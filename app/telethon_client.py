dc792c1ea2c666bf4543d42657073083e53a162c
AVBurashnikov <a.v.burashnikov@gmail.com>
2026-01-30 19:27:46 +0300

    Add teletho_client abd some minor changes

--- app/telethon_client.py
+++ app/telethon_client.py
@@ -1,1 +1,14 @@
+import os
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetHistoryRequest
 
+API_ID = int(os.getenv("TELETHON_API_ID"))
+API_HASH = os.getenv("TELETHON_API_HASH")
+SESSION = os.getenv("TELETHON_SESSION_NAME", "tgpulse_session")
+
+async def get_recent_messages(username: str, limit: int = 10):
+    async with TelegramClient(SESSION, API_ID, API_HASH) as client:
+        entity = await client.get_entity(username)
+        history = await client.get_messages(entity, limit=limit)
+        # messages have .views for channels if available
+        return history
